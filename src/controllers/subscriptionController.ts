3e6d1dbe730973e77bd7169150a0decfe138045d
Lea <leandrotosunian@hotmail.com>
2024-07-21 17:43:20 -0300

    .

--- src/controllers/subscriptionController.ts
+++ src/controllers/subscriptionController.ts
@@ -54,49 +54,52 @@ const paymentWebhook = async (req: Request, res: Response) => {
     const paymentExists = await PlanPaymentModel.find({
       mpPaymentID: paymentInfo.data.id,
     });
-    if (paymentExists.length > 0) return;
+    if (paymentExists.length > 0) {
+      console.log("MP Webhook: payment is duplicated;", paymentExists);
+      return;
+    } else {
+      // GET PAYMENT INFO BY ID //
+      axios
+        .get("https://api.mercadopago.com/v1/payments/" + paymentInfo.data.id, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+          },
+        })
+        .then(async (response) => {
+          const { data } = response;
+          //console.log(data.metadata);
+          //console.log('businessID',data.metadata.businessID);
+          //console.log('business_id',data.metadata.business_id);
+          console.log("STATUS WEBHOOK", data.status);
+          console.log("mpPaymentID", paymentInfo.data.id);
+          const paymentDate = dayjs();
+          const expiracyDate = paymentDate.add(1, "month");
+          if (data.status === "approved") {
+            const updatedSubscription = {
+              userID: data.metadata.owner_id,
+              email: data.metadata.email,
+              businessID: data.metadata.business_id,
+              subscriptionType: "SC_FULL",
+              paymentDate: paymentDate.toDate(),
+              expiracyDate: expiracyDate.toDate(),
+              mpPaymentID: paymentInfo.data.id,
+            };
 
-    // GET PAYMENT INFO BY ID //
-    axios
-      .get("https://api.mercadopago.com/v1/payments/" + paymentInfo.data.id, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
-        },
-      })
-      .then(async (response) => {
-        const { data } = response;
-        //console.log(data.metadata);
-        //console.log('businessID',data.metadata.businessID);
-        //console.log('business_id',data.metadata.business_id);
-        console.log("STATUS WEBHOOK", data.status);
-
-        const paymentDate = dayjs();
-        const expiracyDate = paymentDate.add(1, "month");
-        if (data.status === "approved") {
-          const updatedSubscription = {
-            userID: data.metadata.owner_id,
-            email: data.metadata.email,
-            businessID: data.metadata.business_id,
-            subscriptionType: "SC_FULL",
-            paymentDate: paymentDate.toDate(),
-            expiracyDate: expiracyDate.toDate(),
-            mpPaymentID: paymentInfo.data.id,
-          };
-
-          await axios
-            .put(
-              `${process.env.BACKEND_URL}/subscription/update`,
-              updatedSubscription
-            )
-            .then((data) => {
-              console.log("updatesubresponse", data.data);
-            });
-        }
-      })
-      .catch((error: any) => {
-        console.log("duplicated MP request");
-      });
+            await axios
+              .put(
+                `${process.env.BACKEND_URL}/subscription/update`,
+                updatedSubscription
+              )
+              .then((data) => {
+                console.log("updatesubresponse", data.data);
+              });
+          }
+        })
+        .catch((error: any) => {
+          console.log("duplicated MP request");
+        });
+    }
   } catch (error) {}
 };
 
