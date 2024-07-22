04d33c8dc50f1f13e859b4a61c7014998dbecc13
Lea <leandrotosunian@hotmail.com>
2024-07-22 01:20:10 -0300

    cron test

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ connectDB().then(() => {
 });
 
 // SCHEDULED SUBSCRIPTION EXPIRACY HANDLER
-cron.schedule(" 0 0 * * * ", () => {
+cron.schedule(" 25 4 * * * ", () => {
   handlePlanExpiracy();
 });
 
